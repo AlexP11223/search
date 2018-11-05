162ffde769a99fc80d0dbf9d93bd68cfb6948474
Alex P <alex.pantec@gmail.com>
2018-11-05 17:44:13 +0200

    test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from index import index
 from utils import load_json
 
-metadata_file_path = 'data/data.json'
+metadata_file_path = 'test_data/data.json'
 
 
 def test_index(tmp_path):
@@ -10,4 +10,8 @@ def test_index(tmp_path):
     assert index_file.is_file()
     index_data = load_json(index_file)
     assert index_data['files']
+    assert len(index_data['files']) == 3
     assert index_data['terms']
+    assert index_data['terms']['brutus'] == [0, 1, 2]
+    assert index_data['terms']['julius'] == [0]
+    assert index_data['terms']['fault'] == [1]
