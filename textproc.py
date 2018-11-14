5afdd2aae271ebca588aa4e4d14e28bd0a2f2d4e
Alex P <alex.pantec@gmail.com>
2018-11-14 21:03:57 +0200

    code cleaning

--- textproc.py
+++ textproc.py
@@ -3,6 +3,19 @@ import nltk
 import nltk.corpus
 
 
+stop_words = set(nltk.corpus.stopwords.words('english')).union(
+    # looks like some punctuation symbols are not tagged correctly
+    {'’', '–', '—', '−', '..', '“', '[', ']', '‘', "'", '…', '”', "''", '"', '•'})
+
+stop_tags = {'.',  # sentence terminator (., !, ?)
+             ',',  # comma
+             ':',  # colon or ellipsis (:, ;, ...)
+             '--',  # dash
+             '(', ')',  # parenthesis ((, ), [, ], {, })
+             "'", '`',  # quotation
+             }
+
+
 @lru_cache(maxsize=1)
 def download_nltk_data_if_needed():
     nltk.download('punkt')
@@ -17,16 +30,6 @@ def extract_terms(text):
         return nltk.pos_tag(tokens)
 
     def normalize(tagged_tokens):
-        stop_words = set(nltk.corpus.stopwords.words('english'))
-        # looks like some punctuation symbols are not tagged correctly
-        stop_words = stop_words.union({'’', '–', '—', '−', '..', '“', '[', ']', '‘', "'", '…', '”', "''", '"', '•'})
-        stop_tags = {'.',  # sentence terminator (., !, ?)
-                     ',',  # comma
-                     ':',  # colon or ellipsis (:, ;, ...)
-                     '--',  # dash
-                     '(', ')',  # parenthesis ((, ), [, ], {, })
-                     "'", '`',  # quotation
-                     }
         words = {it[0].lower() for it in tagged_tokens if not it[1] in stop_tags}
         return words.difference(stop_words)
 
