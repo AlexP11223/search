19a37156b97bf43ed437059b990551821783c309
Alex P <alex.pantec@gmail.com>
2019-02-16 18:42:40 +0200

    refactor

--- textproc.py
+++ textproc.py
@@ -14,46 +14,56 @@ def download_nltk_data_if_needed():
     nltk.download('wordnet')
 
 
-def extract_terms(text, lemmatization=True):
-    def tokenize():
-        tokens = nltk.word_tokenize(text)
-        return nltk.pos_tag(tokens)
+def get_stop_words(lang='english'):
+    return set(nltk.corpus.stopwords.words(lang)).union(
+        # looks like some punctuation symbols are not tagged correctly
+        {'’', '–', '—', '−', '..', '“', '[', ']', '‘', "'", '…', '”', "''", '"', '•'})
 
-    def get_wordnet_pos(treebank_tag):
-        if treebank_tag.startswith('J'):
-            return wordnet.ADJ
-        elif treebank_tag.startswith('V'):
-            return wordnet.VERB
-        elif treebank_tag.startswith('N'):
-            return wordnet.NOUN
-        elif treebank_tag.startswith('R'):
-            return wordnet.ADV
-        else:
-            return None
 
-    def lemmatize(tagged_words):
-        wd_tagged_words = {(it[0], get_wordnet_pos(it[1])) for it in tagged_words}
-        lemmatizer = WordNetLemmatizer()
-        return {it[0] if it[1] is None else lemmatizer.lemmatize(it[0], it[1]) for it in wd_tagged_words}
-
-    def normalize(tagged_tokens):
-        stop_words = set(nltk.corpus.stopwords.words('english')).union(
-            # looks like some punctuation symbols are not tagged correctly
-            {'’', '–', '—', '−', '..', '“', '[', ']', '‘', "'", '…', '”', "''", '"', '•'})
-        stop_tags = {'.',  # sentence terminator (., !, ?)
-                     ',',  # comma
-                     ':',  # colon or ellipsis (:, ;, ...)
-                     '--',  # dash
-                     '(', ')',  # parenthesis ((, ), [, ], {, })
-                     "'", '`',  # quotation
-                     }
-        tagged_words = {(it[0].lower(), it[1]) for it in tagged_tokens if not it[1] in stop_tags}
-        if lemmatization:
-            words = lemmatize(tagged_words)
-        else:
-            words = {it[0] for it in tagged_words}
-        return words.difference(stop_words)
-
+def get_stop_tags():
     download_nltk_data_if_needed()
+    return {'.',  # sentence terminator (., !, ?)
+            ',',  # comma
+            ':',  # colon or ellipsis (:, ;, ...)
+            '--',  # dash
+            '(', ')',  # parenthesis ((, ), [, ], {, })
+            "'", '`',  # quotation
+            }
 
-    return normalize(tokenize())
+
+def tokenize(text):
+    download_nltk_data_if_needed()
+    tokens = nltk.word_tokenize(text)
+    return nltk.pos_tag(tokens)
+
+
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+
+
+def lemmatize(tagged_words):
+    wd_tagged_words = {(it[0], get_wordnet_pos(it[1])) for it in tagged_words}
+    lemmatizer = WordNetLemmatizer()
+    return {it[0] if it[1] is None else lemmatizer.lemmatize(it[0], it[1]) for it in wd_tagged_words}
+
+
+def normalize(tagged_tokens, lemmatization=True):
+    tagged_words = {(it[0].lower(), it[1]) for it in tagged_tokens if not it[1] in get_stop_tags()}
+    if lemmatization:
+        words = lemmatize(tagged_words)
+    else:
+        words = {it[0] for it in tagged_words}
+    return words.difference(get_stop_words())
+
+
+def extract_terms(text, lemmatization=True):
+    return normalize(tokenize(text), lemmatization)
